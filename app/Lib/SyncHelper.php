63628a180980eb96ce6ed22df832b125e614cdfd
Burak Doğan <bdogan85@gmail.com>
2015-07-09 16:55:46 +0300

    Update Refresh Token

--- app/Lib/SyncHelper.php
+++ app/Lib/SyncHelper.php
@@ -2,6 +2,8 @@
 
 App::uses('SyncHelperException', 'ReformApi.Lib');
 App::uses('ReformApi', 'ReformApi.Lib');
+App::uses('HttpSocket', 'Network/Http');
+App::uses('CakeTime', 'Utility');
 
 class SyncHelper
 {
@@ -93,11 +95,188 @@ class SyncHelper
     ));
   }
 
+  // Reform Token validated flag
+  private $reformAccessTokenValidated = false;
+
   // Return Reform Access Token from Customer Data
   public function getReformAccessToken()
   {
     $accessToken = Hash::get($this->customer_data, 'AccessToken.0.token');
+    if (!$this->reformAccessTokenValidated)
+    {
+      if ($this->__validateReformToken('access_token', $accessToken) === false)
+      {
+        if ($refreshToken = $this->getReformRefreshToken())
+        {
+          if ($this->__refreshAccessToken($refreshToken, $accessToken))
+          {
+            $this->customer_data['AccessToken'][0]['token'] = $accessToken;
+            $this->customer_data['RefreshToken'][0]['token'] = $refreshToken;
+            $this->reformAccessTokenValidated = true;
+            $this->reformRefreshTokenValidated = true;
+            return $accessToken;
+          }
+        }
+        else
+        {
+          throw new Exception('`ReformRefreshToken` cannot be validated');
+        }
+        $accessToken = false;
+      }
+      $this->reformAccessTokenValidated = true;
+    }
     return ($accessToken ? $accessToken : false);
+  }
+
+  // Reform Refresh Token validated flag
+  private $reformRefreshTokenValidated = false;
+
+  // Return Reform Refresh Token from Customer Data
+  public function getReformRefreshToken()
+  {
+    $refreshToken = Hash::get($this->customer_data, 'RefreshToken.0.token');
+    if (!$this->reformRefreshTokenValidated)
+    {
+      if ($this->__validateReformToken('refresh_token', $refreshToken) === false)
+      {
+        $refreshToken = false;
+      }
+      $this->reformRefreshTokenValidated = true;
+    }
+    return ($refreshToken ? $refreshToken : false);
+  }
+
+  // Get Access Token from Refresh Token
+  private function __refreshAccessToken(&$refreshToken, &$accessToken)
+  {
+
+    // Get AccessToken from Refresh Token
+    // Set Action Url
+    $action = Configure::read('PlatinMarket.OAuth.protocol') . '://' . Configure::read('PlatinMarket.OAuth.host') . Configure::read('PlatinMarket.OAuth.path') . '/access_token.json';
+
+    // Prepare Data
+    $data = array(
+      'platform_uuid' => Configure::read('PlatinMarket.PlatformUUID'),
+      'customer_uuid' => $this->customer_data['Customer']['uuid'],
+      'token' => $refreshToken,
+      'grant_type' => 'refresh_token'
+    );
+
+    $hash_map = implode(',', array_keys($data));
+    $data['hash'] = $this->__hash(Configure::read('PlatinMarket.ClientID'), Configure::read('PlatinMarket.ClientSecret'), $data);
+    $data['hash-map'] = $hash_map;
+    $data['application_uuid'] = Configure::read('PlatinMarket.ApplicationUUID');
+    $data['time'] = CakeTime::toRSS(new DateTime(), 'Europe/Istanbul');
+
+    // Make Request
+    $HttpSocket = new HttpSocket();
+    $response = $HttpSocket->get($action, $data);
+
+    // Check Response
+    if ($response->code != 200)
+    {
+      try { $response_msg = json_decode($response->body, true); } catch (Exception $err) { $response_msg = $response->body; }
+      throw new Exception("Request for 'access_token' from 'refresh_token' failed. " . $response_msg['message'], $response->code);
+      return;
+    }
+
+    // Parse Response
+    try
+    {
+      $response_data = json_decode($response->body, true);
+      $response_data = $response_data['result'];
+      extract($response_data, EXTR_OVERWRITE);
+    }
+    catch (Exception $err)
+    {
+      throw new Exception("Parse failed get 'access_token' from 'refresh_token' failed");
+    }
+
+    // Save Tokens
+    // Prepare Data
+    $accessToken = $access_token;
+    $access_token = array('AccessToken' => array(
+      'token' => $access_token,
+      'lifetime' => $access_token_lifetime,
+      'customer_id' => $this->customer_data['Customer']['id']
+    ));
+
+    // Delete Old Access Token
+    $this->__model('AccessToken')->deleteAll(array('AccessToken.customer_id' => $this->customer_data['Customer']['id']));
+
+    // Save Access Token
+    $this->__model('AccessToken')->create();
+    if (!$this->__model('AccessToken')->save($access_token))
+      throw new Exception("AccessToken create failed");
+
+    // Prepare Data
+    $refreshToken = $refresh_token;
+    $refresh_token = array('RefreshToken' => array(
+      'token' => $refresh_token,
+      'lifetime' => $refresh_token_lifetime,
+      'customer_id' => $this->customer_data['Customer']['id']
+    ));
+
+    // Delete Old Refresh Token
+    $this->__model('RefreshToken')->deleteAll(array('RefreshToken.customer_id' => $this->customer_data['Customer']['id']));
+
+    // Save Refresh Token
+    $this->__model('RefreshToken')->create();
+    if (!$this->__model('RefreshToken')->save($refresh_token))
+      throw new Exception("RefreshToken create failed");
+
+    return true;
+  }
+
+  // Validate Token
+  private function __validateReformToken($grant_type = 'access_token', $token)
+  {
+    // Get AccessToken from Refresh Token
+    // Set Action Url
+    $action = Configure::read('PlatinMarket.OAuth.protocol') . '://' . Configure::read('PlatinMarket.OAuth.host') . Configure::read('PlatinMarket.OAuth.path') . '/validate_token.json';
+
+    // Prepare Data
+    $data = array(
+      'platform_uuid' => Configure::read('PlatinMarket.PlatformUUID'),
+      'customer_uuid' => $this->customer_data['Customer']['uuid'],
+      'token' => $token,
+      'grant_type' => $grant_type
+    );
+
+    $hash_map = implode(',', array_keys($data));
+    $data['hash'] = $this->__hash(Configure::read('PlatinMarket.ClientID'), Configure::read('PlatinMarket.ClientSecret'), $data);
+    $data['hash-map'] = $hash_map;
+    $data['application_uuid'] = Configure::read('PlatinMarket.ApplicationUUID');
+    $data['time'] = CakeTime::toRSS(new DateTime(), 'Europe/Istanbul');
+
+    // Make Request
+    $HttpSocket = new HttpSocket();
+    $response = $HttpSocket->get($action, $data);
+
+    // Check Response
+    if ($response->code != 200)
+    {
+      try { $response_msg = json_decode($response->body, true); } catch (Exception $err) { $response_msg = $response->body; }
+      throw new Exception("Request for '" . $grant_type . "' validation failed. " . $response_msg['message'], $response->code);
+      return;
+    }
+
+    // Parse Response
+    try
+    {
+      $response_data = json_decode($response->body, true);
+      return (Hash::get($response_data, 'result.valid') === true);
+    }
+    catch (Exception $err)
+    {
+      throw new Exception("Parse failed get 'access_token' from 'refresh_token' failed");
+    }
+  }
+
+  // Hash secret data
+  protected function __hash($clientId, $clientSecret, $data, $enc = 'sha256') {
+    $SecurityData = strtoupper(hash($enc, $clientSecret . $clientId, false));
+    return strtoupper(hash($enc, implode('', array_values($data)) . $SecurityData, false));
   }
 
   // Return Setrow Api Key from Customer Data
